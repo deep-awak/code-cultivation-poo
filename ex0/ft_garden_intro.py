56a8636622867a960c39df2269a3e13bb5020078
deep-awak <mahery.razafimanantsoa.dev@gmail.com>
2026-07-23 22:51:09 -1100

    refactor(ex0):asign the value in the variable

--- ex0/ft_garden_intro.py
+++ ex0/ft_garden_intro.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 
-def ft_garden_intro(name: str, height: float, age: int) -> str:
-    return (f"Plant: {name}\nHeight:{height} cm\nAge: {age} days \n")
+def ft_garden_intro() -> str:
+    name: str = "Rose"
+    height: float = 25.0
+    age: int = 30
+    return (f"Plant: {name}\nHeight:{height}cm\nAge: {age} days \n")
 
 
 def main() -> None:
     print("=== Welcome to My garden ===")
-    print(ft_garden_intro("Rose", 25.0, 30))
+    print(ft_garden_intro())
     print("=== End of Program ===")
 
 
